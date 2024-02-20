79e543e2b375f05a73b512dc561d1851837b42c0
Marcus Barros <marcus.mvbs@gmail.com>
2024-02-20 12:03:52 -0500

    Docker-Kind Integration Completed v1.1

--- Docker_config/entrypoint.sh
+++ Docker_config/entrypoint.sh
@@ -2,9 +2,7 @@
 
 # Initialize Kind cluster
 kind create cluster --config /kind-config/kind-config.yaml
-kind get clusters
 #kubectl apply -f kind-config/nginx-service.yaml
-kubectl get services
 
 # Keep the container running
 tail -f /dev/null
